cdeb94f6774b57321d1e11c3f2ec0726ffef932d
ColinJohnson747 <60419260+ColinJohnson747@users.noreply.github.com>
2020-07-05 12:50:08 -0400

    Created header component

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Employee Directory</h1>
+      <p>
+        Welcome to your Employee tracker! Click on the name of each column to
+        sort them. Or use the search bar to narrow down your results.
+      </p>
+    </div>
+  );
+}
+
+export default Header;
